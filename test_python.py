3e1c4383c316307ff2c310b49768679542ef0dc0
Leonel <leonel.arispe@webeando.me>
2022-08-23 13:55:26 -0400

    example file

--- test_python.py
+++ test_python.py
@@ -1,36 +1,45 @@
-def hola():
-    print("Hola desde github")
-
-
-
 if __name__ == '__main__':
     import unittest
-    import io
-
 
     class TestSumaFunction(unittest.TestCase):
-
         def test_suma_15_10(self):
-            self.assertEqual(suma(15,10), 25)
+            self.assertEqual(suma(15, 10), 25)
 
         def test_suma__10_10(self):
-            self.assertEqual(suma(-10,10), 0)
+            self.assertEqual(suma(-10, 10), 0)
 
         def test_suma__2__15(self):
-            self.assertEqual(suma(-2,-15), -17)
+            self.assertEqual(suma(-2, -15), -17)
 
-    def suites():
-        return[
-            TestSumaFunction
-        ]
+        def test_suma_6__15(self):
+            self.assertEqual(suma(6, -15), -57)
 
-    loader = unittest.TestLoader()
-    suite = unittest.TestSuite()
-    stream = io.StringIO()
-    for test_case in suites():
-        suite.addTests(loader.loadTestsFromTestCase(test_case))
-    runner = unittest.TextTestRunner(stream=stream)
-    result = runner.run(suite)
+        def test_suma__2__10(self):
+            self.assertEqual(suma(-2, -10), -17)
 
-    stream.seek(0)
-    print(stream.read())
+        def test_suma__3__10(self):
+            self.assertEqual(suma(-3, -10), -17)
+
+
+def evaluate_code():
+    try:
+        import js
+        import unittest
+        import io
+
+        stream = io.StringIO()
+        suite = unittest.loader.TestLoader().loadTestsFromTestCase(TestSumaFunction)
+        result = unittest.TextTestRunner(stream=stream).run(suite)
+
+        js.results = {
+            'tests_number': result.testsRun,
+            'failed': len(result.failures),
+            'success': result.testsRun - len(result.failures)
+        }
+
+        stream.seek(0)
+        print(stream.read())
+
+    except Exception as err:
+        print(err)
+
