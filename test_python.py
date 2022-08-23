f362f5fec141aad1d5e888063d030f59572e6b53
Leonel <leonel.arispe@webeando.me>
2022-08-23 14:34:53 -0400

    example fix

--- test_python.py
+++ test_python.py
@@ -1,24 +1,24 @@
-if __name__ == '__main__':
-    import unittest
+import unittest
 
-    class TestSumaFunction(unittest.TestCase):
-        def test_suma_15_10(self):
-            self.assertEqual(suma(15, 10), 25)
 
-        def test_suma__10_10(self):
-            self.assertEqual(suma(-10, 10), 0)
+class TestSumaFunction(unittest.TestCase):
+    def test_suma_15_10(self):
+        self.assertEqual(suma(15, 10), 25)
 
-        def test_suma__2__15(self):
-            self.assertEqual(suma(-2, -15), -17)
+    def test_suma__10_10(self):
+        self.assertEqual(suma(-10, 10), 0)
 
-        def test_suma_6__15(self):
-            self.assertEqual(suma(6, -15), -57)
+    def test_suma__2__15(self):
+        self.assertEqual(suma(-2, -15), -17)
 
-        def test_suma__2__10(self):
-            self.assertEqual(suma(-2, -10), -17)
+    def test_suma_6__15(self):
+        self.assertEqual(suma(6, -15), -57)
 
-        def test_suma__3__10(self):
-            self.assertEqual(suma(-3, -10), -17)
+    def test_suma__2__10(self):
+        self.assertEqual(suma(-2, -10), -17)
+
+    def test_suma__3__10(self):
+        self.assertEqual(suma(-3, -10), -17)
 
 
 def evaluate_code():
@@ -42,4 +42,3 @@ def evaluate_code():
 
     except Exception as err:
         print(err)
-
